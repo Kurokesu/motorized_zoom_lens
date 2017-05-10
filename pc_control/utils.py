17b8f78aedcd7962ea441c6defadfd1a7a1d2739
Nick <nick81794@yahoo.com>
2017-05-10 09:32:56 -0700

    Updated to use PyQt5

--- pc_control/utils.py
+++ pc_control/utils.py
@@ -1,32 +1,36 @@
+#!/usr/bin/env/python3
 # -*- coding: utf-8 -*-
 
-__author__      = "Saulius Lukse"
-__copyright__   = "Copyright 2016, kurokesu.com"
+__author__ = "Saulius Lukse"
+__copyright__ = "Copyright 2016, kurokesu.com"
 __version__ = "0.2"
 __license__ = "GPL"
 
 
 import json
-import os
 
 # -----------------------------------------------------------------------
+
+
 def json_boot_routine(file):
-	config = {}
+    config = {}
 
-	with open(file) as f:
-		config = json.load(f)
-		f.close()
+    with open(file) as f:
+        config = json.load(f)
+        f.close()
 
-	config["clean_exit"] = False
-	config["boot_count"] += 1 
+    config["clean_exit"] = False
+    config["boot_count"] += 1
 
-	with open(file, 'w') as f:
-		json.dump(config, f, sort_keys=True, indent=4)
+    with open(file, 'w') as f:
+        json.dump(config, f, sort_keys=True, indent=4)
 
-	return config
+    return config
 
 # -----------------------------------------------------------------------
+
+
 def json_exit_routine(file, config):
-	config["clean_exit"] = True
-	with open(file, 'w') as f:
-		json.dump(config, f, sort_keys=True, indent=4)
+    config["clean_exit"] = True
+    with open(file, 'w') as f:
+        json.dump(config, f, sort_keys=True, indent=4)
